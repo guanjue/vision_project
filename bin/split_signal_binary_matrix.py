eb00d37f42609b0df6ab8dc329898d4641b2ef86
Universe <universe@MacBook-Air.local>
2017-09-22 23:26:40 -0400

    add id col

--- bin/split_signal_binary_matrix.py
+++ bin/split_signal_binary_matrix.py
@@ -22,34 +22,41 @@ def write2d_array(array,output):
 		r1.write(str(records[len(records)-1])+'\n')
 	r1.close()
 ################################################################################################
-def split_signal_binary_matrix(inputfile, output1, output1_col, output2, output2_col, output3, output3_col):
+def split_signal_binary_matrix(inputfile, id_col, output1, output1_col, output2, output2_col, output3, output3_col):
+	print('read data')
 	data_all = read2d_array(inputfile,str)
-
 	### split signal matrix & binary matrix
+	print('split matrix')
+	ids = np.transpose([data_all[:,id_col-1]])
 	interval_info = data_all[:,0:output1_col]
 	signal = data_all[:,output1_col:output2_col]
 	binary = data_all[:,output2_col:output3_col]
+	### paste id
+	interval_info_matrix = np.concatenate((ids, interval_info), axis=1)
+	signal_matrix = np.concatenate((ids, signal), axis=1)
+	binary_matrix = np.concatenate((ids, binary), axis=1)
 	### write results
-	write2d_array(interval_info, output1)
-	write2d_array(signal, output2)
-	write2d_array(binary, output3)
+	print('write data')
+	write2d_array(interval_info_matrix, output1)
+	write2d_array(signal_matrix, output2)
+	write2d_array(binary_matrix, output3)
 
 
 ############################################################################
-#time python split_signal_binary_matrix.py -i homerTable3.peaks.filtered.txt -a homerTable3.peaks.filtered.interval.txt -x 4 -b homerTable3.peaks.filtered.signal.txt -y 32 -c homerTable3.peaks.filtered.binary_pattern.txt -z 60
+#time python split_signal_binary_matrix.py -i homerTable3.peaks.filtered.txt -n 4 -a homerTable3.peaks.filtered.interval.txt -x 4 -b homerTable3.peaks.filtered.signal.txt -y 32 -c homerTable3.peaks.filtered.binary_pattern.txt -z 60
 
 import getopt
 import sys
 def main(argv):
 	try:
-		opts, args = getopt.getopt(argv,"hi:a:b:c:x:y:z:")
+		opts, args = getopt.getopt(argv,"hi:n:a:b:c:x:y:z:")
 	except getopt.GetoptError:
-		print 'time python split_signal_binary_matrix.py -i inputfile a output1 -x output1_col -b output2 -y output2_col -c output3 -z output3_col'
+		print 'time python split_signal_binary_matrix.py -i inputfile -n id_col -a output1 -x output1_col -b output2 -y output2_col -c output3 -z output3_col'
 		sys.exit(2)
 
 	for opt,arg in opts:
 		if opt=="-h":
-			print 'time python split_signal_binary_matrix.py -i inputfile a output1 -x output1_col -b output2 -y output2_col -c output3 -z output3_col'
+			print 'time python split_signal_binary_matrix.py -i inputfile -n id_col -a output1 -x output1_col -b output2 -y output2_col -c output3 -z output3_col'
 			sys.exit()
 		elif opt=="-i":
 			inputfile=str(arg.strip())
@@ -59,6 +66,8 @@ def main(argv):
 			output2=str(arg.strip())		
 		elif opt=="-c":
 			output3=str(arg.strip())
+		elif opt=="-n":
+			id_col=int(arg.strip())
 		elif opt=="-x":
 			output1_col=int(arg.strip())
 		elif opt=="-y":
@@ -66,7 +75,7 @@ def main(argv):
 		elif opt=="-z":
 			output3_col=int(arg.strip())
 
-	split_signal_binary_matrix(inputfile, output1, output1_col, output2, output2_col, output3, output3_col)
+	split_signal_binary_matrix(inputfile, id_col, output1, output1_col, output2, output2_col, output3, output3_col)
 
 if __name__=="__main__":
 	main(sys.argv[1:])
